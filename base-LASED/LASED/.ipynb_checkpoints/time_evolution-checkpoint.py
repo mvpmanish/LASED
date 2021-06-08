df66601b282cf8030b38937748b076a67d24a5bb
mvpmanish <mvpmanish@gmail.com>
2021-06-08 14:26:36 +0100

    Created time evolution with just doppler averaging
    Run and tested in example notebook

--- base-LASED/LASED/.ipynb_checkpoints/time_evolution-checkpoint.py
+++ base-LASED/LASED/.ipynb_checkpoints/time_evolution-checkpoint.py
@@ -45,6 +45,52 @@ def timeEvolution(n, E, G, Q, Q_decay, tau, laser_intensity, laser_wavelength, t
         for g in G:
             for e in E:
                 rho_output[index(g, e, n)][position] = abs(rho_t[index(e, g, n), 0])
+
+def timeEvolutionDopplerAveraging(n, E, G, Q, Q_decay, tau, laser_intensity, laser_wavelength, doppler_width, doppler_detunings, time, rho0, rho_output, tau_f = None, rabi_scaling = None, print_eq = None, pretty_print_eq = None, atomic_velocity = None):
+    
+    if(print_eq or pretty_print_eq != None):
+        print("Cannot print equations when gaussian or doppler averaging!")
+        print_eq = None
+        pretty_print_eq = None
+    
+    # Calculate doppler_spacing
+    d_doppler = abs(doppler_detunings[1] - doppler_detunings[0])
+    doppler_factor = d_doppler/(np.sqrt(2*PI*doppler_width*doppler_width))
+    
+    for doppler_delta in doppler_detunings:
+            # Calculate the half-rabi frequency
+            rabi = halfRabiFreq(laser_intensity, tau, laser_wavelength)
+            
+            A = timeEvolutionMatrix(n, E, G, Q, Q_decay, tau, laser_wavelength, laser_intensity, 
+                        tau_f = tau_f, rabi_scaling = rabi_scaling, symbolic_print = pretty_print_eq, 
+                                    numeric_print = print_eq, atomic_velocity = atomic_velocity)
+
+            # Compute the diagonalised matrix D and matrix of eigenvectors V
+            D = diagonalise(A)
+            V = matrixOfEigenvec(A)
+            f = np.dot(la.inv(V), rho0)  # Compute V^-1*rho(0)
+
+            # Calculate the exponential
+            for position, t in enumerate(time, start = 0):
+                # Use expm() which computes the matrix exponential using the Pade approximation
+                VexpDt = np.dot(V, sparsela.expm(D*t))  # Use sparse linear algebra to compute e^Dt as this is faster
+                rho_t = np.dot(VexpDt, f)
+
+                # Append density matrix for each ring and each detuning fraction
+                # rho(t)_ee
+                for e in E:
+                    rho_output[index(e, e, n)][position] += doppler_factor*np.exp(-np.power(doppler_delta/doppler_width, 2)/2)*(abs(rho_t[index(e, e, n), 0]))
+                # rho(t)_gg
+                for g in G:
+                    rho_output[index(g, g, n)][position] += doppler_factor*np.exp(-np.power(doppler_delta/doppler_width, 2)/2)*(abs(rho_t[index(g, g, n), 0]))
+                # rho(t)_eg
+                for e in E:
+                    for g in G:
+                        rho_output[index(e, g, n)][position] += doppler_factor*np.exp(-np.power(doppler_delta/doppler_width, 2)/2)*(abs(rho_t[index(e, g, n), 0]))
+                #rho(t)_ge
+                for g in G:
+                    for e in E:
+                        rho_output[index(g, e, n)][position] += doppler_factor*np.exp(-np.power(doppler_delta/doppler_width, 2)/2)*(abs(rho_t[index(e, g, n), 0]))
     
 
 def timeEvolutionGaussianAndDopplerAveraging(n, E, G, Q, Q_decay, tau, laser_power, r_sigma, n_intensity, laser_wavelength, doppler_width, doppler_detunings, time, rho0, rho_output, tau_f = None, rabi_scaling = None, print_eq = None, pretty_print_eq = None, atomic_velocity = None):
