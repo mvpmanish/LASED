9344fade837a8cc313638e53c605e81d5ca77123
mvpmanish <mvpmanish@gmail.com>
2021-06-24 16:22:19 +0100

    Added nuclear spin to State class

--- base-LASED/LASED/state.py
+++ base-LASED/LASED/state.py
@@ -5,10 +5,12 @@ Class definition for an atomic state
 # Class to hold information to construct an atomic state
 class State():
     # Parameterised constructor
-    def __init__(self, L, S, m, w, label):
+    def __init__(self, L, S, I, m, w, label):
         self.L = L  # Orbital angular momentum quantum number
         self.S = S  # Spin quantum number
-        self.m = m  # m_J degeneracy of total angular momentum
+        self.m = m  # m is the degeneracy of total angular momentum
         self.J = L+S  # Total angular momentum
+        self.I = I  # Nuclear spin quantum number
+        self.F = I + self.J
         self.w = w  # angular frequency of state
         self.label = label  # number labelling of state e.g. state |1> would have label 1
