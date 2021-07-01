d0dabbd22cf13aac1ecd4f139ed02a740818525e
mvpmanish <mvpmanish@gmail.com>
2021-07-01 16:39:43 +0100

    Updated State to calculate hyperfine if none given
    State class will accept all quantum numbers to define a state. If none given then they will be calculated from the regular expressions: J = L + S and F = J + I

--- base-LASED/LASED/state.py
+++ base-LASED/LASED/state.py
@@ -5,12 +5,26 @@ Class definition for an atomic state
 # Class to hold information to construct an atomic state
 class State():
     # Parameterised constructor
-    def __init__(self, L, S, I, m, w, label):
+    def __init__(self, label, w, m, L, S, J = None, I = None, F = None):
+        self.label = label  # number labelling of state e.g. state |1> would have label 1
+        self.w = w  # angular frequency of state in Grad/s
+        
         self.L = L  # Orbital angular momentum quantum number
         self.S = S  # Spin quantum number
         self.m = m  # m is the degeneracy of total angular momentum
-        self.J = L+S  # Total angular momentum
-        self.I = I  # Nuclear spin quantum number
-        self.F = I + self.J
-        self.w = w  # angular frequency of state in Grad/s
-        self.label = label  # number labelling of state e.g. state |1> would have label 1
+        
+        if(J):
+            self.J = J  # Coupling between L and S
+        else:
+            self.J = L+S 
+        if(I):
+            self.I = I  # Nuclear spin quantum number
+        else:
+            self.I = 0  
+        if(F):
+            self.F = F  # Total angular momentum, coupling between J and I
+        else:
+            self.F = self.J + self.I
+         
+        
+        
