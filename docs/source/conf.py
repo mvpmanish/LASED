6f645b4293e7b06b9c47ea83dad60c3e3a6d63a3
mvpmanish <mvpmanish@gmail.com>
2021-09-20 13:16:13 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,6 @@ import os
 import sys
 import sphinx_rtd_theme
 import nbsphinx
-import jupyter_sphinx
 sys.path.insert(0, os.path.abspath('../../base-LASED/LASED'))
 
 
@@ -33,7 +32,7 @@ release = '0.3'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'nbsphinx', 'jupyter_sphinx.execute']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'nbsphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
