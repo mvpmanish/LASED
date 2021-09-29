d936757499284248f05758f706d610a21988f368
mvpmanish <mvpmanish@gmail.com>
2021-09-29 12:08:43 +0100

    Changed version to 0.3.2 and fixed Tutorial4 equation

--- base-LASED/setup.py
+++ base-LASED/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.3.1' 
+VERSION = '0.3.2'
 DESCRIPTION = 'LASED'
 LONG_DESCRIPTION = 'A Laser-Atom Interaction Simulator using Quantum Electrodynamics'
 
