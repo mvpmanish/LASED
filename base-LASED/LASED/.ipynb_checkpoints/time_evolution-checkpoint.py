3d693e63f800a080f72148e3419134885b6b4a12
mvpmanish <mvpmanish@gmail.com>
2022-01-31 16:30:46 +0000

    Added tau_b
    - Added decay from ground state to non-laser excited manifold
    - Changed time evolution matrix and symbolic print eq

--- base-LASED/LASED/.ipynb_checkpoints/time_evolution-checkpoint.py
+++ base-LASED/LASED/.ipynb_checkpoints/time_evolution-checkpoint.py
@@ -12,32 +12,32 @@ from LASED.time_evolution_matrix import *
 import numpy as np
 import scipy.linalg as la
 
-def timeEvolution(n, E, G, Q, Q_decay, tau, laser_intensity, laser_wavelength, time, rho0, rho_output, tau_f = None, detuning = None, rabi_scaling = None, rabi_factors = None, print_eq = None, pretty_print_eq = None, atomic_velocity = None):
+def timeEvolution(n, E, G, Q, Q_decay, tau, laser_intensity, laser_wavelength, time, rho0, rho_output, tau_f = None, tau_b = None, detuning = None, rabi_scaling = None, rabi_factors = None, print_eq = None, pretty_print_eq = None, atomic_velocity = None):
     """Calculates the time evolution of a laser-atom system.
-    
+
     Uses a flattened density matrix rho0 and calculates the time evolution over the time specified.
     The density matrix at each time step is stored in rho_output.
     """
-    
+
     rabi = halfRabiFreq(laser_intensity, tau, laser_wavelength)
-            
-    A = timeEvolutionMatrix(n, E, G, Q, Q_decay, tau, laser_wavelength, laser_intensity, 
-                        tau_f = tau_f, detuning = detuning, rabi_scaling = rabi_scaling,
-                        rabi_factors = rabi_factors, symbolic_print = pretty_print_eq, 
+
+    A = timeEvolutionMatrix(n, E, G, Q, Q_decay, tau, laser_wavelength, laser_intensity,
+                        tau_f = tau_f, tau_b = tau_b, detuning = detuning, rabi_scaling = rabi_scaling,
+                        rabi_factors = rabi_factors, symbolic_print = pretty_print_eq,
                         numeric_print = print_eq, atomic_velocity = atomic_velocity)
 
     # Compute the diagonalised matrix D and matrix of eigenvectors V
     D = diagonalise(A)
     V = matrixOfEigenvec(A)
     f = np.dot(la.inv(V), rho0)  # Compute V^-1*rho(0)
-    
+
     # Calculate the exponential
     for position, t in enumerate(time, start = 0):
         # Use expm() which computes the matrix exponential using the Pade approximation
         expS = la.expm(D*t) # Compute exp(D*t), this takes the longest so needs speeding up
-        VexpDt = np.dot(V, expS)  
+        VexpDt = np.dot(V, expS)
         rho_t = np.dot(VexpDt, f)
-        
+
         # Append density matrix elements
         # rho(t)_ee
         for e in E:
@@ -56,29 +56,39 @@ def timeEvolution(n, E, G, Q, Q_decay, tau, laser_intensity, laser_wavelength, t
             for e in E:
                 rho_output[index(g, e, n)][position] = rho_t[index(g, e, n), 0]
 
-def timeEvolutionDopplerAveraging(n, E, G, Q, Q_decay, tau, laser_intensity, laser_wavelength, doppler_width, doppler_detunings, time, rho0, rho_output, tau_f = None, detuning = None, rabi_scaling = None, rabi_factors = None, print_eq = None, pretty_print_eq = None, atomic_velocity = None):
+def timeEvolutionDopplerAveraging(n, E, G, Q, Q_decay, tau, laser_intensity, laser_wavelength, doppler_width, doppler_detunings, time, rho0, rho_output, tau_f = None, tau_b = None, detuning = None, rabi_scaling = None, rabi_factors = None, print_eq = None, pretty_print_eq = None, atomic_velocity = None):
     """Calculates the time evolution of a laser-atom system with a Gaussian doppler profile for the atoms.
-    
+
     Uses a flattened density matrix rho0 and calculates the time evolution over the time specified.
-    The density matrix at each time step is stored in rho_output. 
+    The density matrix at each time step is stored in rho_output.
     """
-    
+
     if(print_eq or pretty_print_eq != None):
         print("Cannot print equations when beam profile or doppler averaging!")
         print_eq = None
         pretty_print_eq = None
-    
-    # Calculate doppler_spacing
-    d_doppler = abs(doppler_detunings[1] - doppler_detunings[0])
-    doppler_factor = d_doppler/(np.sqrt(2*PI*doppler_width*doppler_width))
-    
-    for doppler_delta in doppler_detunings:
+
+    # Calculate doppler scaler which scales byu the number of atoms in the
+    # doppler distribution
+    doppler_scaler= 1/(np.sqrt(2*PI*doppler_width*doppler_width))
+
+    for i,doppler_delta in enumerate(doppler_detunings, start = 0):
             # Calculate the half-rabi frequency
             rabi = halfRabiFreq(laser_intensity, tau, laser_wavelength)
-            
-            A = timeEvolutionMatrix(n, E, G, Q, Q_decay, tau, laser_wavelength, laser_intensity, 
-                        tau_f = tau_f, detuning = detuning, rabi_scaling = rabi_scaling, 
-                        rabi_factors = rabi_factors, symbolic_print = pretty_print_eq, 
+            # Calculate the Doppler factor from the spacing between doppler detunings
+            if(i == 0):
+                d_doppler = abs(doppler_detunings[i+1] - doppler_detunings[i])
+            else:
+                d_doppler = abs(doppler_detunings[i] - doppler_detunings[i-1])
+            doppler_factor = d_doppler*doppler_scaler
+            #Substitute in the detuning
+            if(detuning):
+                doppler_detuning =  detuning + doppler_delta
+            else:
+                doppler_detuning = doppler_delta
+            A = timeEvolutionMatrix(n, E, G, Q, Q_decay, tau, laser_wavelength, laser_intensity,
+                        tau_f = tau_f, tau_b = tau_b, detuning = doppler_detuning, rabi_scaling = rabi_scaling,
+                        rabi_factors = rabi_factors, symbolic_print = pretty_print_eq,
                         numeric_print = print_eq, atomic_velocity = atomic_velocity)
 
             # Compute the diagonalised matrix D and matrix of eigenvectors V
@@ -109,30 +119,30 @@ def timeEvolutionDopplerAveraging(n, E, G, Q, Q_decay, tau, laser_intensity, las
                 for g in G:
                     for e in E:
                         rho_output[index(g, e, n)][position] += doppler_factor*np.exp(-np.power(doppler_delta/doppler_width, 2)/2)*(rho_t[index(e, g, n), 0])
-                        
-def timeEvolutionGaussianAveraging(n, E, G, Q, Q_decay, tau, laser_power, r_sigma, n_intensity, laser_wavelength, time, rho0, rho_output, tau_f = None, detuning = None, rabi_scaling = None, rabi_factors = None, print_eq = None, pretty_print_eq = None, atomic_velocity = None):
+
+def timeEvolutionGaussianAveraging(n, E, G, Q, Q_decay, tau, laser_power, r_sigma, n_intensity, laser_wavelength, time, rho0, rho_output, tau_f = None, tau_b = None, detuning = None, rabi_scaling = None, rabi_factors = None, print_eq = None, pretty_print_eq = None, atomic_velocity = None):
     """Calculates the time evolution of a laser-atom system with a Gaussian laser beam profile.
-    
+
     Uses a flattened density matrix rho0 and calculates the time evolution over the time specified.
-    The density matrix at each time step is stored in rho_output. 
+    The density matrix at each time step is stored in rho_output.
     """
-    
+
     if(print_eq or pretty_print_eq != None):
         print("Cannot print equations when beam profile or doppler averaging!")
         print_eq = None
         pretty_print_eq = None
-    
+
     # Create rings of laser beam to integrate populations over
     R = np.linspace(r_sigma, 3*r_sigma, n_intensity)
-    
+
     for k, r in enumerate(R, start = 0):
             # Calculate the half-rabi frequency
             laser_intensity = gaussianIntensity(laser_power, r_sigma, r)
             rabi = halfRabiFreq(laser_intensity, tau, laser_wavelength)
-            
-            A = timeEvolutionMatrix(n, E, G, Q, Q_decay, tau, laser_wavelength, laser_intensity, 
-                        tau_f = tau_f, rabi_scaling = rabi_scaling, rabi_factors = rabi_factors, 
-                        detuning = detuning, symbolic_print = pretty_print_eq, 
+
+            A = timeEvolutionMatrix(n, E, G, Q, Q_decay, tau, laser_wavelength, laser_intensity,
+                        tau_f = tau_f, tau_b = tau_b, rabi_scaling = rabi_scaling, rabi_factors = rabi_factors,
+                        detuning = detuning, symbolic_print = pretty_print_eq,
                         numeric_print = print_eq, atomic_velocity = atomic_velocity)
 
             # Compute the diagonalised matrix D and matrix of eigenvectors V
@@ -163,36 +173,46 @@ def timeEvolutionGaussianAveraging(n, E, G, Q, Q_decay, tau, laser_power, r_sigm
                 for g in G:
                     for e in E:
                         rho_output[index(g, e, n)][position] += (2*k+1)*(rho_t[index(e, g, n), 0])/(n_intensity*n_intensity)
-    
 
-def timeEvolutionGaussianAndDopplerAveraging(n, E, G, Q, Q_decay, tau, laser_power, r_sigma, n_intensity, laser_wavelength, doppler_width, doppler_detunings, time, rho0, rho_output, tau_f = None, detuning = None, rabi_scaling = None, rabi_factors = None, print_eq = None, pretty_print_eq = None, atomic_velocity = None):
+
+def timeEvolutionGaussianAndDopplerAveraging(n, E, G, Q, Q_decay, tau, laser_power, r_sigma, n_intensity, laser_wavelength, doppler_width, doppler_detunings, time, rho0, rho_output, tau_f = None, tau_b = None, detuning = None, rabi_scaling = None, rabi_factors = None, print_eq = None, pretty_print_eq = None, atomic_velocity = None):
     """Calculates the time evolution of a laser-atom system with a Gaussian doppler profile for the atoms and a Gaussian laser beam profile.
-    
+
     Uses a flattened density matrix rho0 and calculates the time evolution over the time specified.
-    The density matrix at each time step is stored in rho_output. 
+    The density matrix at each time step is stored in rho_output.
     """
-    
+
     if(print_eq or pretty_print_eq != None):
         print("Cannot print equations when beam profile or doppler averaging!")
         print_eq = None
         pretty_print_eq = None
-    
+
     # Create rings of laser beam to integrate populations over
     R = np.linspace(r_sigma, 3*r_sigma, n_intensity)
-    
-    # Calculate doppler_spacing
-    d_doppler = abs(doppler_detunings[1] - doppler_detunings[0])
-    doppler_factor = d_doppler/(np.sqrt(2*PI*doppler_width*doppler_width))
-    
-    for doppler_delta in doppler_detunings:
+
+    # Calculate doppler scaler which scales byu the number of atoms in the
+    # doppler distribution
+    doppler_scaler = 1/(np.sqrt(2*PI*doppler_width*doppler_width))
+
+    for i,doppler_delta in enumerate(doppler_detunings, start = 0):
         for k, r in enumerate(R, start = 0):
             # Calculate the half-rabi frequency
             laser_intensity = gaussianIntensity(laser_power, r_sigma, r)
             rabi = halfRabiFreq(laser_intensity, tau, laser_wavelength)
-            
-            A = timeEvolutionMatrix(n, E, G, Q, Q_decay, tau, laser_wavelength, laser_intensity, 
-                        tau_f = tau_f, rabi_scaling = rabi_scaling, rabi_factors = rabi_factors, 
-                        detuning = detuning, symbolic_print = pretty_print_eq, 
+            # Calculate the Doppler factor from the spacing between doppler detunings
+            if(i == 0):
+                d_doppler = abs(doppler_detunings[i+1] - doppler_detunings[i])
+            else:
+                d_doppler = abs(doppler_detunings[i] - doppler_detunings[i-1])
+            doppler_factor = d_doppler*doppler_scaler
+            #Substitute in the detuning
+            if(detuning):
+                doppler_detuning =  detuning + doppler_delta
+            else:
+                doppler_detuning = doppler_delta
+            A = timeEvolutionMatrix(n, E, G, Q, Q_decay, tau, laser_wavelength, laser_intensity,
+                        tau_f = tau_f, tau_b = tau_b, rabi_scaling = rabi_scaling, rabi_factors = rabi_factors,
+                        detuning = doppler_detuning, symbolic_print = pretty_print_eq,
                         numeric_print = print_eq, atomic_velocity = atomic_velocity)
 
             # Compute the diagonalised matrix D and matrix of eigenvectors V
@@ -223,4 +243,3 @@ def timeEvolutionGaussianAndDopplerAveraging(n, E, G, Q, Q_decay, tau, laser_pow
                 for g in G:
                     for e in E:
                         rho_output[index(g, e, n)][position] += doppler_factor*np.exp(-np.power(doppler_delta/doppler_width, 2)/2)*(2*k+1)*(rho_t[index(e, g, n), 0])/(n_intensity*n_intensity)
-                                                                                                     
--- base-LASED/LASED/.ipynb_checkpoints/time_evolution_matrix-checkpoint.py
+++ base-LASED/LASED/.ipynb_checkpoints/time_evolution_matrix-checkpoint.py
@@ -16,7 +16,7 @@ from sympy import Symbol
 import numpy as np
 
 def timeEvolutionMatrix(n, E, G, Q, Q_decay, tau, laser_wavelength, laser_intensity,
-                        tau_f = None, detuning = None, symbolic_print = None, numeric_print = None,
+                        tau_f = None, tau_b = None, detuning = None, symbolic_print = None, numeric_print = None,
                        rabi_scaling = None, rabi_factors = None, atomic_velocity = None):
     """Function to create and populate the coupled differential equation matrix A for the laser-atom system.
     
@@ -45,20 +45,20 @@ def timeEvolutionMatrix(n, E, G, Q, Q_decay, tau, laser_wavelength, laser_intens
         atomic_velocity = 0
     
     # Populate A matrix
-    rho_ggpp(A, n, E, G, Q, Q_decay, tau, rabi, rabi_factors, numeric_print = numeric_print)
+    rho_ggpp(A, n, E, G, Q, Q_decay, tau, rabi, rabi_factors, tau_b = tau_b, numeric_print = numeric_print)
     rho_eepp(A, n, E, G, Q, Q_decay, tau, rabi, rabi_factors, tau_f = tau_f, numeric_print = numeric_print)
-    rho_ge(A, n, E, G, Q, Q_decay, tau, rabi, rabi_factors, laser_wavelength, atomic_velocity, tau_f = tau_f, detuning = detuning, numeric_print = numeric_print)
-    rho_eg(A, n, E, G, Q, Q_decay, tau, rabi, rabi_factors, laser_wavelength, atomic_velocity, tau_f = tau_f, detuning = detuning, numeric_print = numeric_print)
+    rho_ge(A, n, E, G, Q, Q_decay, tau, rabi, rabi_factors, laser_wavelength, atomic_velocity, tau_f = tau_f, tau_b = tau_b, detuning = detuning, numeric_print = numeric_print)
+    rho_eg(A, n, E, G, Q, Q_decay, tau, rabi, rabi_factors, laser_wavelength, atomic_velocity, tau_f = tau_f, tau_b = tau_b, detuning = detuning, numeric_print = numeric_print)
 
     # Symbolic Printing
     if(symbolic_print == True):
         init_printing()
-        symbolicPrintSystem(n, E, G, Q, Q_decay, tau_f, detuning, 
+        symbolicPrintSystem(n, E, G, Q, Q_decay, tau_f, tau_b, detuning, 
                             laser_wavelength, atomic_velocity, rabi_scaling, rabi_factors)
     
     return A
 
-def rho_ggpp(A, n, E, G, Q, Q_decay, tau, rabi, rabi_factors, numeric_print = None):
+def rho_ggpp(A, n, E, G, Q, Q_decay, tau, rabi, rabi_factors, tau_b = None, numeric_print = None):
     """ Function to populate the matrix A with coefficients for populations and atomic coherences of the ground states. 
     """
     # rho_gg''
@@ -66,6 +66,8 @@ def rho_ggpp(A, n, E, G, Q, Q_decay, tau, rabi, rabi_factors, numeric_print = No
         for gpp in G:
             row = index(g, gpp, n)  # matrix positions of rho_gg'' in 1D array
             A[row, row] += -1.j*delta(g, gpp)  # first term in equation
+            if(tau_b != None):
+                A[row, row] -= 1/tau_b
             for e in E:
                 column = index(g, e, n)
                 for i,q in enumerate(Q):  # Sum over all polarisations
@@ -92,7 +94,6 @@ def rho_ggpp(A, n, E, G, Q, Q_decay, tau, rabi, rabi_factors, numeric_print = No
                             decay_const = generalisedDecayConstant(ep, epp, gpp, G, Q_decay)/(2*tau)  # Divide by two to take into account double counting of e'e'' and e''e'
                             A[row, column] += decay_const
                             A[row, column2] += decay_const
-                        
             if(numeric_print == True):  # Print numerical equations
                 print("rho_dot", g.label, gpp.label, " = ")
                 for line in range(n*n):
@@ -123,7 +124,7 @@ def rho_eepp(A, n, E, G, Q, Q_decay, tau, rabi, rabi_factors, tau_f = None, nume
                     if (A[row, line] != 0):
                         print(A[row, line], "rho", getStateLabelsFromLineNo(line, n))
 
-def rho_ge(A, n, E, G, Q, Q_decay, tau, rabi, rabi_factors, laser_wavelength, atomic_velocity, tau_f = None, detuning = None, numeric_print = None):
+def rho_ge(A, n, E, G, Q, Q_decay, tau, rabi, rabi_factors, laser_wavelength, atomic_velocity, tau_f = None, tau_b = None, detuning = None, numeric_print = None):
     """ Function to populate the matrix A with coefficients for optical coherences between ground and excited states. 
     """
     # rho_ge
@@ -136,6 +137,8 @@ def rho_ge(A, n, E, G, Q, Q_decay, tau, rabi, rabi_factors, laser_wavelength, at
                 A[row, row] += -1.j*detuning
             if(tau_f != None):
                 A[row, row] -= 1/(2*tau_f)
+            if(tau_b != None):
+                A[row, row] -= 1/(2*tau_b)
             for ep in E:
                 column = index(ep, e, n)
                 for i,q in enumerate(Q): 
@@ -150,7 +153,7 @@ def rho_ge(A, n, E, G, Q, Q_decay, tau, rabi, rabi_factors, laser_wavelength, at
                     if (A[row, line] != 0):
                         print(A[row, line], "rho", getStateLabelsFromLineNo(line, n))
 
-def rho_eg(A, n, E, G, Q, Q_decay, tau, rabi, rabi_factors, laser_wavelength, atomic_velocity, tau_f = None, detuning = None, numeric_print = None):
+def rho_eg(A, n, E, G, Q, Q_decay, tau, rabi, rabi_factors, laser_wavelength, atomic_velocity, tau_f = None, tau_b = None, detuning = None, numeric_print = None):
     """ Function to populate the matrix A with coefficients for optical coherences between excited and ground states. 
     """
     # rho_eg
@@ -163,6 +166,8 @@ def rho_eg(A, n, E, G, Q, Q_decay, tau, rabi, rabi_factors, laser_wavelength, at
                 A[row, row] += 1.j*detuning
             if(tau_f != None):
                 A[row, row] -= 1/(2*tau_f)
+            if(tau_b != None):
+                A[row, row] -= 1/(2*tau_b)
             for ep in E:
                 column = index(e, ep, n)
                 for i,q in enumerate(Q): 
