d8a83ac90f6ba32a04d1e9be96c0feaa4c190d7e
user34523303 <46034994+user34523303@users.noreply.github.com>
2022-03-30 21:09:36 +0100

    Add files via upload

--- base-LASED/setup.py
+++ base-LASED/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup, find_packages
+from pybind11 import get_cmake_dir # Available at setup time due to pyproject.toml
+from pybind11.setup_helpers import Pybind11Extension, build_ext
 
 VERSION = '1.0.1'
 DESCRIPTION = 'LASED'
@@ -6,6 +8,33 @@ LONG_DESCRIPTION = 'A Laser-Atom Interaction Simulator using Quantum Electrodyna
 
 with open("../README.md", "r") as fh:
     long_description = fh.read()
+
+
+
+
+
+# Setup the C++ module
+
+ext_modules = [
+    Pybind11Extension("LASED.CppLASED",
+        ["src/main.cpp"],
+        include_dirs = ["include"],
+        define_macros = [('VERSION_INFO', VERSION)],
+        ),
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Setting up
 setup(
@@ -21,10 +50,11 @@ setup(
         install_requires=[
             "numpy >= 1.20",
             "scipy >= 1.6.0",
-            "sympy >= 1.8"
+            "sympy >= 1.8",
+            "pybind11 >= 2.8.1" # package that resuqired for the cpp module
         ], # add any additional packages that
         # needs to be installed along with your package. Eg: 'caer'
-
+        ext_modules=ext_modules, # Setup the C++ module
         keywords=['python', 'laser-atom', 'simulation', 'quantum', 'quantum electrodynamics', 'physics'],
         classifiers= [
             "Development Status :: 5 - Production/Stable",
